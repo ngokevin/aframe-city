9ef5cc02a8f872b383edae71b4d83a0d78e014d4
Kevin Ngo <me@ngokevin.com>
2016-03-12 00:24:41 -0800

    The Street

---
+++ components/geometry-merge.js
@@ -0,0 +1,34 @@
+/* Provides global scope for keeping track of layers of merged geometries. */
+AFRAME.registerSystem('geometry-merge', {
+  init: function () {
+    this.layers = {};
+  },
+
+  getOrCreateLayer: function (layerName) {
+    var layers = this.layers;
+
+    // Return existing geometry.
+    if (layers[layerName]) {
+      return layers[layerName];
+    }
+
+    // Create base geometry.
+    layers[layerName] = new THREE.Geometry();
+    return layers[layerName];
+  }
+});
+
+/* Merge geometries by specified layers. */
+AFRAME.registerComponent('geometry-merge', {
+  dependencies: ['geometry'],
+
+  schema: {
+    default: ''
+  },
+
+  init: function () {
+    var layerName = this.data;
+    THREE.GeometryUtils.merge(this.system.getOrCreateLayer(layerName),
+                              this.el.getObject3D('mesh').geometry);
+  }
+});
